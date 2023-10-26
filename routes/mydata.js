87a5ebb493dd798a1ed998a4c2e046afa86045d9
srinivasmane09 <manesrinivas11@gmail.com>
2023-10-25 22:28:16 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'srinivas mane' });
+  res.render('mydata', { title: 'srinivas mane' });
 });
 
 module.exports = router;
