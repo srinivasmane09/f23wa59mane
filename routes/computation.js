c64ddab741c48a6abf39265ce2c89e95f9ca1cb6
srinivasmane09 <manesrinivas11@gmail.com>
2023-10-25 22:48:07 -0500

    Create computation.js

---
+++ routes/computation.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams
+    if (req.method === 'GET') {
+        value = search_params.get("x")
+        if(value === null)
+        {
+            value=Math.round(Math.random()*999);
+        }     
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        
+        
+        res.write('<br>Math.abs() applied to ' + value + ' is ' + Math.abs(value))
+        res.write('Math.acos() applied to ' + value + ' is ' + Math.acos(value));
+        res.write('<br>Math.sin() applied to ' + value + ' is ' + Math.sin(value))
+        res.write('Math.sinh() applied to ' + value + ' is ' + Math.sinh(2,value));
+    
+
+        res.end()
+    }
+
+});
+
+module.exports = router;
